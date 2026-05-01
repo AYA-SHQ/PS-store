9f0912ad4db40cebaf5e1cd097154a5d5a821ee2
DELL <DELL@DESKTOP-ICE8Q88>
2026-05-02 01:01:26 +0300

    ForgetPass screen

---
+++ src/PS_store/ForgetPassScreen.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PS_store;
+
+/**
+ *
+ * @author DELL
+ */
+public class ForgetPassScreen {
+    
+    public static void main(String[] args) {
+        
+    }
+}
