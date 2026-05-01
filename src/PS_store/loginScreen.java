9927f77ac38ac0318f07e3dbaf4b7cc809388f02
DELL <DELL@DESKTOP-ICE8Q88>
2026-05-02 00:54:11 +0300

    first commit

---
+++ src/PS_store/loginScreen.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PS_store;
+
+/**
+ *
+ * @author DELL
+ */
+public class loginScreen {
+    
+    public static void main(String[] args) {
+        System.out.print("");
+    }
+   
+}
